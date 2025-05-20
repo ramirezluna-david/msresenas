d89d9a100aa378b1fa2e72b1901e86192df13442
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-19 21:25:39 -0400

    first commit

---
+++ src/main/java/com/edutech/msresenas/MsresenasApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msresenas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsresenasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsresenasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/msresenas/MsresenasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msresenas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsresenasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
